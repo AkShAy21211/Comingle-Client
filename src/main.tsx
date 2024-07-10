07c7f75f1bb84ba123e4aa5a6a9a386469f49f27
AkShAy21211 <akshay68580@gmail.com>
2024-07-11 03:41:09 +0530

    hosted url chanages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.ts";
 import ToastProvider from "./Components/Common/ToastProvider.tsx";
-import { registerServiceWorker } from './serviceWorkerRegistration.ts';
+import { registerServiceWorker } from '../serviceWorkerRegistration.ts';
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
