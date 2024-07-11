00a7f5e2638ca232df9234acca0df0ebe2e2278f
AkShAy21211 <akshay68580@gmail.com>
2024-07-11 07:40:35 +0530

    hosted url chanages

--- src/Apis/socket.ts
+++ src/Apis/socket.ts
@@ -2,7 +2,7 @@ import socketIOClient from "socket.io-client";
 const backendUrl =
   import.meta.env.VITE_NODE_ENV === "DEVELOPMENT"
     ? import.meta.env.VITE_BACKEND_URI_DEV
-    : import.meta.env.VITE_BACKEND_URL;
+    : import.meta.env.VITE_BACKEND_URI;
 
 const ws = backendUrl;
 const socket = socketIOClient(ws);
